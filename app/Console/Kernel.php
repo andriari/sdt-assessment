62d3daee07de200cec11eda534401d2d03a3b734
andriariwibowo <andri.ariwibowo@bulp.co>
2024-02-25 03:27:49 +0700

    Kernel Scheduler

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,28 +4,23 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
-use App\Models\Customer;
+use App\Services\TaskService;
 
 class Kernel extends ConsoleKernel
 {
     /**
      * Define the application's command schedule.
      */
+
     protected function schedule(Schedule $schedule): void
     {
-        //birthday task
-        $userBirthday = $this->customer->whereDay('dob', now()->day)
-            ->whereMonth('dob', now()->month)
-        ->get();
+        $schedule->call(function () {
+            TaskService::checkBirthdayTask();
+        })->dailyAt('00:00');
 
-        foreach ($userBirthday as $user) {
-            $schedule->call(function () use ($user) {
-                $response = Http::post('https://email-service.digitalenvision.com.au/send-email', [
-                    "email" => $user->email,
-                    "message" => "Hey, ".$user->name." it's your birthday!"
-                ]);
-            })->dailyAt('9:00')->timezone($user->timezone);
-        }
+        $schedule->call(function () {
+            TaskService::sendTask();
+        })->everyThirtyMinutes();
     }
 
     /**
